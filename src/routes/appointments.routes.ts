5473b0abb22216c90252cbd1a654d1d2a60ada97
Luiz Serra <lserra.trade@gmail.com>
2020-06-23 18:45:24 -0300

    validando datas

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,22 +1,41 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(appointment.date, parsedDate),
+    );
+
+    if (findAppointmentInSameDate) {
+        return response.status(400).json({
+            error: 'Não é possível gravar dois agendamentos com a mesma data',
+        });
+    }
+
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     };
 
     appointments.push(appointment);
 
-    response.json(appointment);
+    return response.json(appointment);
 });
 
 export default appointmentRouter;
