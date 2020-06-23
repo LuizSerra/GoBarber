57f60d3abcd79a8bab1e5730822125df590bdc7e
Luiz Serra <lserra.trade@gmail.com>
2020-06-23 12:19:00 -0300

    configurando o debug

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) => {
-  response.json({ message: 'Hello GoStack' });
+    response.json({ message: 'Hello GoStack' });
 });
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333');
-});
+app.listen(3333);
