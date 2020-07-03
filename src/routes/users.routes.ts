2bb422db8ebf4b7e6b5ca0dd5908d5f7b8ffca8b
Luiz Serra <lserra.trade@gmail.com>
2020-07-02 23:02:33 -0300

    adicionando upload de arquivo

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
-
+const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
     try {
         const { name, email, password } = request.body;
@@ -16,5 +19,15 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        console.log(request.file.filename);
+        return response.json({ ok: true });
+    },
+);
 
 export default usersRouter;
