64b8af6ca89c7c9e07cc6d5237213e89448bc7c5
Luiz Serra <lserra.trade@gmail.com>
2020-07-04 20:33:04 -0300

    Adicionando classe de erro

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
     name: string;
@@ -15,7 +16,7 @@ class CreateUserService {
             where: { email },
         });
         if (checkedUser) {
-            throw Error('This e-mail is already used');
+            throw new AppError('This e-mail is already used');
         }
 
         const hashedPassword = await hash(password, 8);
--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import fs from 'fs';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
 
 interface Request {
     user_id: string;
@@ -15,7 +16,10 @@ class UpdateUserAvatarService {
         const userRepository = getRepository(User);
         const user = await userRepository.findOne(user_id);
         if (!user) {
-            throw new Error('Only authenticated users can change the avatar');
+            throw new AppError(
+                'Only authenticated users can change the avatar',
+                401,
+            );
         }
 
         if (user.avatar) {
