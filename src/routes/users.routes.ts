444fe2a887e21342edba69bc56116857314ac3e1
Luiz Serra <lserra.trade@gmail.com>
2020-07-05 15:19:56 -0300

    adicionado a lib express-async-errors para eliminar os try-catch das rotas

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -8,17 +8,13 @@ import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-        const createUserService = new CreateUserService();
+    const createUserService = new CreateUserService();
 
-        const user = await createUserService.execute({ name, email, password });
-        delete user.password;
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    const user = await createUserService.execute({ name, email, password });
+    delete user.password;
+    return response.json(user);
 });
 
 usersRouter.patch(
@@ -26,18 +22,14 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        try {
-            const updateAvatarService = new UpdateUserAvatarService();
-            const user = await updateAvatarService.execute({
-                user_id: request.user.id,
-                avatarFilename: request.file.filename,
-            });
-            delete user.password;
+        const updateAvatarService = new UpdateUserAvatarService();
+        const user = await updateAvatarService.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
+        delete user.password;
 
-            return response.json(user);
-        } catch (e) {
-            return response.status(400).json({ error: e.message });
-        }
+        return response.json(user);
     },
 );
 
