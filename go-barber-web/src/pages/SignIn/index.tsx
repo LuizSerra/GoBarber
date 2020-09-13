b7c9152c3acd607827a24245b0fdfedcc642e27f
Luiz Serra <lserra.trade@gmail.com>
2020-09-13 17:09:24 -0300

    criando a função de logout

--- go-barber-web/src/pages/SignIn/index.tsx
+++ go-barber-web/src/pages/SignIn/index.tsx
@@ -9,7 +9,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 interface SignInFormData {
   email: string;
