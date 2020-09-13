11e82239f38faf6f0718ea1b79dec2a224d749d0
Luiz Serra <lserra.trade@gmail.com>
2020-09-13 13:30:21 -0300

    criando o contexto de autenticação

--- go-barber-web/src/App.tsx
+++ go-barber-web/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/index';
 import SignUp from './pages/SignUp/index';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Luiz' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
--- go-barber-web/src/pages/SignIn/index.tsx
+++ go-barber-web/src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import getValidationErrors from '../../utils/getValidationErrors';
+
+import AuthContext from '../../context/AuthContext';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -13,6 +15,8 @@ import { Container, Content, Background } from './styles';
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
+  const auth = useContext(AuthContext);
+  console.log(auth);
   const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
